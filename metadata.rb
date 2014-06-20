ae999969ea9fe8a52d439dd080afd45c5b8a3b30
Matt Stratton <matt.stratton@gmail.com>
2014-06-20 12:48:52 -0700

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jbruso@trueblue.com'
 license          'We need no stinking license.'
 description      'Installs/Configures directorylister'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.13'
+version          '0.1.14'
 
 depends "windows", "~> 1.31.0"
 depends "iis" , "~> 2.1.2"
