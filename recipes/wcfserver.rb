59d3477baa41e2d6a412280d6004d7b50d4fe413
jbruso <jbruso@trueblue.com>
2014-06-24 13:53:01 -0700

    Added WCF functionality

--- recipes/wcfserver.rb
+++ recipes/wcfserver.rb
@@ -20,3 +20,28 @@ template "#{node[:directorylister][:wcf_root]}directorylister/web.config" do
 
 		})
 end
+
+#creates a new app pool
+iis_pool 'directorylister' do
+    runtime_version "4.0"
+    pipeline_mode :Integrated
+    pool_username node[:directorylister][:apppool_user]
+    pool_password node[:directorylister][:apppool_password]
+    action [:add, :config]
+end
+
+iis_site 'directorylister' do
+	protocol :http
+	port 80
+	path "#{node[:directorylister][:wcf_root]}/directorylister"
+	application_pool 'directorylister'
+	action [:add, :config, :start]
+end
+
+template "#{node[:directorylister][:wcf_root]}directorylister/web.config" do
+	source "web.config.wcfserver.erb"
+	variables({
+		:wcf_server => node[:directorylister][:wcf_server]
+
+		})
+end
