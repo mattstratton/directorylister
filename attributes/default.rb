251982d1bfd200561281f0476b5e6f9129af1960
jbruso <jbruso@trueblue.com>
2014-06-24 15:24:49 -0700

    Changed path on XML file.

--- attributes/default.rb
+++ attributes/default.rb
@@ -21,8 +21,8 @@
 default[:directorylister][:apppool_user] = "directorylister-pool"
 default[:directorylister][:apppool_password] = "PoolAdmin123"
 default[:directorylister][:web_root] = "c:/webroot/"
-default[:directorylister][:wcf_root] = "c:/wcfroot/"
+default[:directorylister][:wcf_root] = "c:/wcfroot/directorylister"
 default[:directorylister][:log_root] = "c:/logs/"
 default[:directorylister][:wcf_server] = "localhoster:19234"
-default[:directorylister][:xmlpath] = "c:/directorylister/lookupservice/"
+default[:directorylister][:xmlpath] = "c:/directorylister/"
 #TODO - factor out port to a separate attribute maybe
