d417cf6dfd3d3703fda5d50046fdf002f74301d8
jbruso <jbruso@trueblue.com>
2014-07-01 16:45:32 -0700

    Moving to default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,12 +14,81 @@ windows_feature 'NetFx4Extended-ASPNET45' do
   all true
 end
 
+windows_feature 'Application-Server' do
+	action :install
+	all true
+end
+
+windows_feature 'IIS-NetFxExtensibility45' do
+	action :install
+end
+
+windows_feature 'NetFx4Extended-ASPNET45' do
+	action :install
+end
+
+windows_feature 'Application-Server-TCP-Port-Sharing' do
+	action :install
+	all true
+end
+
+windows_feature 'Application-Server-WebServer-Support' do
+	action :install
+	all true
+end
+
+windows_feature 'Application-Server-WAS-Support' do
+	action :install
+	all true
+end
+
+windows_feature 'Application-Server-HTTP-Activation' do
+	action :install
+	all true
+end
+
 windows_feature 'IIS-WebServerRole' do
   action :install
   all true
 end
 
 windows_feature 'MSMQ-Services' do
+	action :install
+	all true
+end
+
+windows_feature 'IIS-ISAPIExtensions' do
+	action :install
+	all true
+end
+
+windows_feature 'IIS-ISAPIFilter' do
+	action :install
+	all true
+end
+
+windows_feature 'IIS-ASPNET' do
+	action :install
+	all true
+end
+
+
+windows_feature 'WCF-HTTP-Activation' do
+	action :install
+	all true
+end
+
+windows_feature 'WAS-ProcessModel' do
+	action :install
+	all true
+end
+
+windows_feature 'WAS-WindowsActivationService' do
+	action :install
+	all true
+end
+
+windows_feature 'WAS-ConfigurationAPI' do
 	action :install
 	all true
 end
